535f03f375a6c595ba90c0a50c358249e6fd12e1
David Corral Allo <92435904+davidcorralallo@users.noreply.github.com>
2023-05-08 15:58:10 +0200

    Creada la Base de datos

---
+++ concesionario.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 08-05-2023 a las 13:57:27
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `concesionario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contraseña` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `coches`
+--
+
+DROP TABLE IF EXISTS `coches`;
+CREATE TABLE IF NOT EXISTS `coches` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marca` enum('Audi','Bmw','Cupra','Dacia','Ferrari','Fiat','Ford','Honda','Hyundai','Jaguar','Jeep','Kia','LandRover','Lexus','Maserati','Mazda','Mercedes','Mini','Nissan','Peugeot','Porche','Renault','Seat','Tesla','Toyota','Volskwagen','Lamborghini','') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modelo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `potencia` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cambio` enum('Manual','Automático') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `traccion` enum('Delantera','Trasera','Integral') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `combustible` enum('Gasolina','Diésel','Eléctrico') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `plazas` int NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `img` varchar(155) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `concesionario`
+--
+
+DROP TABLE IF EXISTS `concesionario`;
+CREATE TABLE IF NOT EXISTS `concesionario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `localizacion` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_empleado` int NOT NULL,
+  `id_coche` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_empleado` (`id_empleado`,`id_coche`),
+  KEY `relacion_a_coches` (`id_coche`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `puesto` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `salario` decimal(10,2) NOT NULL,
+  `fecha_contrato` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfiles`
+--
+
+DROP TABLE IF EXISTS `perfiles`;
+CREATE TABLE IF NOT EXISTS `perfiles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `perfil` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarioperfil`
+--
+
+DROP TABLE IF EXISTS `usuarioperfil`;
+CREATE TABLE IF NOT EXISTS `usuarioperfil` (
+  `idUsuario` int NOT NULL,
+  `idPerfil` int NOT NULL,
+  PRIMARY KEY (`idUsuario`,`idPerfil`),
+  KEY `idUsuario` (`idUsuario`,`idPerfil`),
+  KEY `relacion_a_perfiles` (`idPerfil`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+DROP TABLE IF EXISTS `ventas`;
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int NOT NULL,
+  `id_coche` int NOT NULL,
+  `fecha_venta` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_coche` (`id_coche`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `concesionario`
+--
+ALTER TABLE `concesionario`
+  ADD CONSTRAINT `relacion_a_coches` FOREIGN KEY (`id_coche`) REFERENCES `coches` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `relacion_a_empleados` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `usuarioperfil`
+--
+ALTER TABLE `usuarioperfil`
+  ADD CONSTRAINT `relacion_a_perfiles` FOREIGN KEY (`idPerfil`) REFERENCES `perfiles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `relacion_a_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `clientes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
+  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`id_coche`) REFERENCES `coches` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
